017863f70eeca8412da5e298687122b05ed5f4bf
Arno Coetzee <arno.coietzee@simplyworkflow.com>
2023-05-17 16:08:12 +0100

    added database migrations, seeders etc

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Blog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Blog::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'blog_title' => $this->faker->sentence(10),
+            'blog_text' => $this->faker->paragraph(),
+            'publication_datetime' => $this->faker->dateTimeBetween('-3 months', 'now'),
+        ];
+    }
+}
